91c791735be740999509374712c4930277d3bc9c
KyungSeok <leeseok8347@gmail.com>
2018-01-01 19:30:56 +0900

    Initial commit

---
+++ app/src/main/java/mycoin/co/kr/mycoin/MainActivity.kt
@@ -0,0 +1,12 @@
+package mycoin.co.kr.mycoin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
